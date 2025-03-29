f58ea7ee4f61a7eef8386e0fd12b241b0132111d
Camelia Ciuca <camelia1414@gmail.com>
2025-03-29 14:44:51 +0100

    login feature, scenario 1, failed

--- features/login.feature
+++ features/login.feature
@@ -7,3 +7,8 @@ Feature: User Login
     Given I am on the login page
     When I enter username "user1" and password "pass123"
     Then I should be logged in successfully
+
+  Scenario: Failed login with invalid credentials
+    Given I am on the login page
+    When I enter username "user1" and password "wrongpass"
+    Then I should see an error "Invalid username or password"
--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -1,15 +1,22 @@
 from behave import given, when, then
+from user import User,AuthenticationError
+
 
 @given(u'I am on the login page')
-def step_on_login_page(context):
-    raise NotImplementedError(u'STEP: Given I am on the login page')
+def step_on_login_page(context, username, password):
+    context.user = User(username="user1", password="pass123")
 
-
-@when(u'I enter username "user1" and password "pass123"')
-def step_enter_valid_credentials(context):
-    raise NotImplementedError(u'STEP: When I enter username "user1" and password "pass123"')
-
+@when(u'I enter {username} and {password}')
+def step_enter_valid_credentials(context,username, password):
+    try:
+        context.user.login(username,password)
+    except AuthenticationError:
+        context.error = str(e)
 
 @then(u'I should be logged in successfully')
 def step_check_logged_in(context):
-    raise NotImplementedError(u'STEP: Then I should be logged in successfully')
+    assert context.user.is_authenticate() is True, "User should be logged in"
+
+@then('I should see an error "{error_message}"')
+def step_check_error(context, error_message):
+    assert context.error == error_message, f"Expected error: {error_message}"
